7fd20e5d45ed63c7014f94ca1f6be3e736d93ff4
cying111 <chen_ying@gis.a-star.edu.sg>
2020-05-22 12:17:17 +0000

    Updated bamFileNames

--- inst/script/data.R
+++ inst/script/data.R
@@ -4,7 +4,7 @@
 # save(sample_info, file = "sample_info.RData")
 
 
-# bamFileNames = dir("./inst/extdata/", pattern = '.bam$')
+bamFileNames = dir("./inst/extdata/", pattern = '.bam$')
 # save(bamFileNames, file = "./data/bamFileNames.RData")
 
 
@@ -13,5 +13,11 @@
 # system(paste0("grep ^22  ",gtf.file," > ",new_gtf.file))
 # annotation.file <- "Homo_sapiens.GRCh38.91_chr22.gtf"
 # devtools::install_github("GoekeLab/bambu",force = TRUE)
-# annotationGrangesList <- bambu::prepareAnnotationsFromGTF(annotation.file)
-# save(annotationGrangesList, file = "annotationGrangesList.RData")
+# annotationGrangesList <- prepareAnnotationsFromGTF(annotation.file)
+# gr <-GRanges(seqnames = "22",
+#              ranges = IRanges(1, 25409234),
+#              strand = "+")
+# hits <- findOverlaps(annotationGrangesList, gr, ignore.strand = TRUE)
+# annotationGrangesList_tmp <- annotationGrangesList[queryHits(hits)]
+# annotationGrangesList_chr22_1_25409234 <- annotationGrangesList_tmp
+# save(annotationGrangesList_chr22_1_25409234, file = "annotationGrangesList_chr22_1_25409234.RData")
