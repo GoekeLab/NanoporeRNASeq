8e14d28c7e9d42b5b50d06b2c7412d4839945ae1
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-06-23 20:09:00 +0800

    use save(version=2) for creating RData files

--- inst/script/make-data.R
+++ inst/script/make-data.R
@@ -7,21 +7,23 @@
 library(NanoporeRNASeq)
 sample.file <- system.file("extdata","sample_information.rds", package = "NanoporeRNASeq")
 sample_info <- readRDS(sample.file)
-save(sample_info, file = "sample_info.RData")
+save(sample_info, file = "./data/sample_info.RData", version = 2)
 
 #create bamFileNames.RData
 bamFileNames = dir("./inst/extdata/", pattern = '.bam$')
-save(bamFileNames, file = "./data/bamFileNames.RData")
+save(bamFileNames, file = "./data/bamFileNames.RData", version = 2)
 
 #Download the Homo_sapiens.GRCh38.91.gtf from ensembl ftp
 download.file(url = "ftp://ftp.ensembl.org/pub/release-91/gtf/homo_sapiens/Homo_sapiens.GRCh38.91.gtf.gz",
               destfile = "Homo_sapiens.GRCh38.91.gtf.gz")
 #create annotationGrangesList_chr22_1_25409234.RData
 gtf.file <- "Homo_sapiens.GRCh38.91.gtf"
+wkdir <- getwd()
 new_gtf.file <- paste0(wkdir, "/Homo_sapiens.GRCh38.91_chr",22,".gtf")
 system(paste0("grep ^22  ",gtf.file," > ",new_gtf.file))
 annotation.file <- "Homo_sapiens.GRCh38.91_chr22.gtf"
 devtools::install_github("GoekeLab/bambu",force = TRUE)
+library(bambu)
 annotationGrangesList <- prepareAnnotationsFromGTF(annotation.file)
 gr <-GRanges(seqnames = "22",
               ranges = IRanges(1, 25409234),
@@ -29,7 +31,7 @@ gr <-GRanges(seqnames = "22",
 hits <- findOverlaps(annotationGrangesList, gr, ignore.strand = TRUE)
 annotationGrangesList_tmp <- annotationGrangesList[queryHits(hits)]
 annotationGrangesList_chr22_1_25409234 <- annotationGrangesList_tmp
-save(annotationGrangesList_chr22_1_25409234, file = "annotationGrangesList_chr22_1_25409234.RData")
+save(annotationGrangesList_chr22_1_25409234, file = "annotationGrangesList_chr22_1_25409234.RData", version = 2)
 #create annotationGrangesList_chr22_1_34500000.RData
 gr <-GRanges(seqnames = "22",
              ranges = IRanges(1, 34500000),
@@ -37,4 +39,4 @@ gr <-GRanges(seqnames = "22",
 hits <- findOverlaps(annotationGrangesList, gr, ignore.strand = TRUE)
 annotationGrangesList_tmp <- annotationGrangesList[queryHits(hits)]
 annotationGrangesList_chr22_1_34500000 <- annotationGrangesList_tmp
-save(annotationGrangesList_chr22_1_34500000, file = "annotationGrangesList_chr22_1_34500000.RData")
+save(annotationGrangesList_chr22_1_34500000, file = "annotationGrangesList_chr22_1_34500000.RData", version = 2)
