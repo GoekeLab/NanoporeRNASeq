fbc0c376e904462ce8d5d906234078d63b8f707f
cying111 <chen_ying@gis.a-star.edu.sg>
2020-07-24 05:40:17 +0000

    add fileNames as an additional column to sample_info and remove bamFiles object

--- inst/script/make-data.R
+++ inst/script/make-data.R
@@ -6,15 +6,14 @@
 #create sample_info.RData
 library(NanoporeRNASeq)
 sample_info <- DataFrame(readRDS(system.file("extdata","sample_information.rds", package = "NanoporeRNASeq")))
+sample_info$fileNames <- list.files(system.file("extdata", package="NanoporeRNASeq"), pattern = ".bam$") 
 save(sample_info, file = "./data/sample_info.RData", version = 2)
 
-#create bamFiles.RData
-bamFiles <- list.files(system.file("extdata", package="NanoporeRNASeq"), pattern = ".bam$")
-save(bamFiles, file = "./data/bamFiles.RData", version = 2)
 
 #Download the Homo_sapiens.GRCh38.91.gtf from ensembl ftp
 download.file(url = "ftp://ftp.ensembl.org/pub/release-91/gtf/homo_sapiens/Homo_sapiens.GRCh38.91.gtf.gz",
               destfile = "Homo_sapiens.GRCh38.91.gtf.gz")
+
 #create annotationGrangesList_chr22_1_25409234.RData
 gtf.file <- "Homo_sapiens.GRCh38.91.gtf"
 wkdir <- getwd()
