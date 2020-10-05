594dd2d5add45f1b26430a91b242c72f3d13b941
cying111 <chen_ying@gis.a-star.edu.sg>
2020-10-05 13:26:27 +0800

    updated make-data script in calling bamfiles

--- inst/script/make-data.R
+++ inst/script/make-data.R
@@ -5,14 +5,26 @@
 
 #create sample_info.RData
 library(NanoporeRNASeq)
-sample_info <- DataFrame(readRDS(system.file("extdata","sample_information.rds", package = "NanoporeRNASeq")))
-sample_info$fileNames <- list.files(system.file("extdata", package="NanoporeRNASeq"), pattern = ".bam$") 
+sample_info <-
+    DataFrame(readRDS(system.file("extdata","sample_information.rds",
+        package = "NanoporeRNASeq")))
+replicate_names <- c("K562_directcDNA_replicate1",
+    "K562_directcDNA_replicate4",
+    "K562_directRNA_replicate6",
+    "MCF7_directcDNA_replicate1",
+    "MCF7_directcDNA_replicate3",
+    "MCF7_directRNA_replicate4")
+bamFileNames <- paste0("NanoporeRNASeq/version0.99.1/",
+        replicate_names,".bam")
+sample_info$fileNames <- bamFileNames
 save(sample_info, file = "./data/sample_info.RData", version = 2)
 
 
 #Download the Homo_sapiens.GRCh38.91.gtf from ensembl ftp
-download.file(url = "ftp://ftp.ensembl.org/pub/release-91/gtf/homo_sapiens/Homo_sapiens.GRCh38.91.gtf.gz",
-              destfile = "Homo_sapiens.GRCh38.91.gtf.gz")
+download.file(url = 
+    "ftp://ftp.ensembl.org/pub/release-91/gtf/homo_sapiens/
+    Homo_sapiens.GRCh38.91.gtf.gz",
+    destfile = "Homo_sapiens.GRCh38.91.gtf.gz")
 
 #create annotationGrangesList_chr22_1_25409234.RData
 gtf.file <- "Homo_sapiens.GRCh38.91.gtf"
@@ -20,12 +32,13 @@ wkdir <- getwd()
 new_gtf.file <- paste0(wkdir, "/Homo_sapiens.GRCh38.91_chr",22,".gtf")
 system(paste0("grep ^22  ",gtf.file," > ",new_gtf.file))
 annotation.file <- "Homo_sapiens.GRCh38.91_chr22.gtf"
+
+
 devtools::install_github("GoekeLab/bambu",force = TRUE)
 library(bambu)
-annotationGrangesList <- prepareAnnotationsFromGTF(annotation.file)
-gr <-GRanges(seqnames = "22",
-              ranges = IRanges(1, 25409234),
-              strand = "+")
+annotationGrangesList <- prepareAnnotations(annotation.file)
+gr <- GRanges(seqnames = "22", ranges = IRanges(1, 25409234),
+    strand = "+")
 hits <- findOverlaps(annotationGrangesList, gr, ignore.strand = TRUE)
 annotationGrangesList_tmp <- annotationGrangesList[queryHits(hits)]
 annotation <- annotationGrangesList_tmp
